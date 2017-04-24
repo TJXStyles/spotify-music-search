6d7e08417b8a241e24ee3a5c946ff778dd24b0c9
Jack Cooc <jack.cooc@gmail.com>
2017-04-24 13:38:53 -0400

    map over artist genre, add comma in between if not the last in the array of genres

--- src/Profile.js
+++ src/Profile.js
@@ -4,14 +4,26 @@ import './App.css';
 class Profile extends Component {
   render() {
     console.log('this.props', this.props)
-    let artist = {name: '', followers: {total: ''}};
-      if (this.props.artist !== null) {
-        artist = this.props.artist;
-      }
+    let artist = {name: '', followers: {total: ''}, images: [{url: ''}], genres: []};
+     artist = this.props.artist !== null ? this.props.artist : artist;
+
     return (
-      <div>
-        <div>{ artist.name }</div>
-        <div>{ artist.followers.total}</div>
+      <div className="profile">
+        <img className="profile-img"
+          src={artist.images[0].url}
+        />
+        <div>Artist: { artist.name }</div>
+        <div>Followers: { artist.followers.total }</div>
+        <div>
+          {
+            artist.genres.map((genre, k) => {
+              genre = genre !== artist.genres[artist.genres.length -1 ] ? ` ${genre},` : `& ${genre}`
+              return (
+                <span key={k}> {genre}</span>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
