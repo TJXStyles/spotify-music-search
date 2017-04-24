e5d48b2b213b3137c5f267846cf745b2b8399d70
Jack Cooc <jack.cooc@gmail.com>
2017-04-24 12:35:10 -0400

    add the react bootstrap componenets to html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <div className="App-title">Music Master</div>
-          <input placeholder="Search for an artist"/>
-          <button>Submit</button>
-        </div>
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+          <FormControl 
+            type="text" 
+            placeholder="Search for an Artist"
+          />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+
+          </InputGroup>
+        </FormGroup>
         <div className="Profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
