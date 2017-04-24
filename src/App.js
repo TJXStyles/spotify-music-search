77ad421bcdc19fa0528ad9fd00268a2a55b932dc
Jack Cooc <jack.cooc@gmail.com>
2017-04-24 12:23:05 -0400

    add skeleton components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div>
+          <div className="App-title">Music Master</div>
+          <input placeholder="Search for an artist"/>
+          <button>Submit</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
